b7c3be61d26cf7288868dfe47c0525e17b8a70b9
davidparkersnowflake <123914934+davidparkersnowflake@users.noreply.github.com>
2023-01-31 19:45:27 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,18 +29,25 @@ streamlit.dataframe(fruits_to_show);
 #--------------------------------------------------------------------
 # display fruity vice api response
 streamlit.header("Fruityvice Fruit Advice!");
-# Add a Text Entry Box and Send the Input to Fruityvice as Part of the API Call
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon");
-#streamlit.text(fruityvice_response.json());
+try:
 
-# write your own comment -what does the next line do? 
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# write your own comment - what does this do?
-streamlit.dataframe(fruityvice_normalized)
+    # Add a Text Entry Box and Send the Input to Fruityvice as Part of the API Call
+    fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 
+    if not fruit_choice:
+        #streamlit.write('The user entered ', fruit_choice)
+    else:
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice);
+        #streamlit.text(fruityvice_response.json());
+
+        # write your own comment -what does the next line do? 
+        fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+        # write your own comment - what does this do?
+        streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+      streamlit.error()
+        
 #--------------------------------------------------------------------------
 
 streamlit.stop()
