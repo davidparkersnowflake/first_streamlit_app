cc106f700d4a217b6b62c64d1c827a28f04eb0d8
davidparkersnowflake <123914934+davidparkersnowflake@users.noreply.github.com>
2023-01-31 20:11:38 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -61,7 +61,7 @@ except URLError as e:
 #streamlit.stop()
 
 def get_fruit_load_list():
-    with my_cnx.cursor as my_cur:
+    with my_cnx.cursor() as my_cur:
         my_cur.execute("SELECT * from pc_rivery_db.public.fruit_load_list");
         return my_cur.fetchall();
 
